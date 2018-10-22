59d60def25952ec16ed5a6aef5c2d594995167de
caveira <leandro.furlan89@gmail.com>
2018-10-22 15:01:18 -0300

    Helm deployment

--- kubernetes/app/app.js
+++ kubernetes/app/app.js
@@ -4,7 +4,11 @@ const port = 3000;
 
 app.listen(port);
 console.log(`Aplicação teste executando em http://localhost: ${port}`);
+
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'candidato';
   res.send(`Olá ${name}!`);
+});
+app.get('/health-check',(req,res)=> {
+ res.send("OK");
 });
--- kubernetes/run.sh
+++ kubernetes/run.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+# Set up Minikube
 eval $(minikube docker-env)
 minikube addons enable ingress
+
+# Builds docker image
 docker build -t idwall-app .
-kubectl create -f service.yaml || kubectl apply -f service.yaml
-kubectl create -f deploy.yaml || kubectl apply -f deploy.yaml
-kubectl create -f ingress.yaml || kubectl apply -f ingress.yaml
-if ! grep -q "idwall-app.local.com" /etc/hosts; then
-	echo "$(minikube ip) idwall-app.local.com" | sudo tee -a /etc/hosts
+
+# Initiate pods
+kubectl create -f idwall-chart/templates/namespace.yaml || kubectl apply -f idwall-chart/templates/namespace.yaml
+kubectl create -f idwall-chart/templates/service.yaml || kubectl apply -f idwall-chart/templates/service.yaml
+kubectl create -f idwall-chart/templates/deploy.yaml || kubectl apply -f idwall-chart/templates/deploy.yaml
+kubectl create -f idwall-chart/templates/ingress.yaml || kubectl apply -f idwall-chart/templates/ingress.yaml
+
+# Set host on current machine
+host=$(kubectl get ingresses.extensions --namespace=idwall-app --no-headers | awk {'print $2'})
+
+if ! grep -q "$host" /etc/hosts; then
+	echo "$(minikube ip) $host" | sudo tee -a /etc/hosts
 fi
+
+# Prints host to operator
+LIGHT_GREEN="\033[1;32m"
+NC="\033[0m"
+echo ""
+echo -e "${LIGHT_GREEN}Please access: $host${NC}"
